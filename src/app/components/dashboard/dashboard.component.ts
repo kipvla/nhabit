2c8b4e97b47ad7311f3701652373894c57d736ab
Kip Riecken <kip.riecken@gmail.com>
2021-06-11 14:04:49 -0400

    update dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: 'app-dashboardtwo',
+  templateUrl: './dashboardtwo.component.html',
+  styleUrls: ['./dashboardtwo.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  isSignedIn = false;
+
+  isWorking = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(localStorage.getItem('user'));
+    if (localStorage.getItem('user')) this.isSignedIn = true;
+    else this.isSignedIn = false;
   }
 
 }
