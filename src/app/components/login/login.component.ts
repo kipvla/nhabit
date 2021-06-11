51f85e087592806ff3febd306e511fbd7af7a23e
Kip Riecken <kip.riecken@gmail.com>
2021-06-11 14:03:57 -0400

    move code to login component and style

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isSignedIn = false;
+  user: User = {
+    uid: '',
+    email: '',
+    displayName: '',
+    photoURL: '',
+    emailVerified: false,
+  };
+
+  loginError = '';
+
+  constructor(public firebaseService: FirebaseService) { }
 
   ngOnInit(): void {
+  }
+
+  async onLogin(email: string, password: string) {
+    await this.firebaseService.login(email, password).catch(err => this.loginError = err.message)
+    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
+    if (this.firebaseService.userData) this.user = this.firebaseService.userData;
   }
 
 }
