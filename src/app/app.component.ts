205b9c2aa1c445bebc56b681943b1c9d7d76dc53
Kip Riecken <kip.riecken@gmail.com>
2021-06-10 20:10:18 -0400

    base app component files with changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { ApiClientService } from './api-client.service';
+import { User } from './models/user';
+import { FirebaseService } from './services/firebase.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nHabit';
+
+  isSignedIn = false;
+  user: User = {
+    uid: '',
+    email: '',
+    displayName: '',
+    photoURL: '',
+    emailVerified: false,
+  };
+
+  registerError = '';
+  loginError = '';
+
+  constructor(
+    private apiClientService: ApiClientService,
+    public firebaseService: FirebaseService
+    ) { }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('user')) this.isSignedIn = true;
+    else this.isSignedIn = false;
+  }
+
+  async onRegister(email: string, password: string) {
+    await this.firebaseService.register(email, password).catch(err => this.registerError = err.message)
+    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
+    if (this.firebaseService.userData) this.user = this.firebaseService.userData;
+  }
+
+  async onLogin(email: string, password: string) {
+    await this.firebaseService.login(email, password).catch(err => this.loginError = err.message)
+    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
+    if (this.firebaseService.userData) this.user = this.firebaseService.userData;
+  }
+
+  handleLogout() {
+    this.isSignedIn = false;
+    this.user = {
+      uid: '',
+      email: '',
+      displayName: '',
+      photoURL: '',
+      emailVerified: false,
+    };
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { FirebaseService } from './services/firebase.service';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [FirebaseService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
