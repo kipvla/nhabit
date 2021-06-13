c31c1c6e4697d11d22dfff11ccfcbcc5e4274f9b
Kip Riecken <kip.riecken@gmail.com>
2021-06-13 14:23:11 -0400

    correct displayName update and remove logout function and console.logs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,6 +16,7 @@ export class HomeComponent implements OnInit {
   userData: any;
 
   @Output() isLogout = new EventEmitter<void>()
+
   constructor(
     public firebaseService: FirebaseService,
     private apiClientService: ApiClientService,
@@ -31,24 +32,19 @@ export class HomeComponent implements OnInit {
       if (res && res.uid) {
         this.uid = res.uid;
         console.log('user is logged in');
-        console.log(this.uid);
-        console.log(res);
       } else {
         console.log('user not logged in');
       }
     })
   }
-  logout() {
-    this.firebaseService.logout()
-    this.isLogout.emit()
-    this.router.navigate(['/dashboard'])
-  }
 
   async addUsername(username: string) {
-    console.log('uid is ', this.userData.uid);
     this.firebaseService.updateProfile({displayName: username});
     if (this.userData.uid) return await this.apiClientService.findUserByUidAndAddDisplayName(this.userData.uid, username)
-    .subscribe(res => this.userName = res.displayName);
+    .subscribe(res => {
+      this.userName = res.displayName
+      this.router.navigate(['/home'])
+    });
     else return null;
   }
 
