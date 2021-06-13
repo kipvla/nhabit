846ce6512efc7779fd1917557fe24bd8e7a3538a
Kip Riecken <kip.riecken@gmail.com>
2021-06-13 14:17:54 -0400

    add game component

---
+++ src/app/components/game/game.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+
+  title: string | null;
+
+  constructor(private activatedRoute: ActivatedRoute) { 
+    this.title = this.activatedRoute.snapshot.paramMap.get('title');
+    console.log(this.title)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
