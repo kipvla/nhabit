1bf162058c3e607312f922f605721d0144a93007
Kip Riecken <kip.riecken@gmail.com>
2021-06-14 12:30:43 -0400

    add routing to login for better ux on login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
@@ -20,15 +21,19 @@ export class LoginComponent implements OnInit {
 
   loginError = '';
 
-  constructor(public firebaseService: FirebaseService) { }
+  constructor(public firebaseService: FirebaseService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   async onLogin(email: string, password: string) {
-    await this.firebaseService.login(email, password).catch(err => this.loginError = err.message)
-    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
-    if (this.firebaseService.userData) this.user = this.firebaseService.userData;
+    await this.firebaseService.login(email, password).then(() => {
+      console.log('logged in');
+      this.router.navigate(['/home'])
+    }).catch(err => this.loginError = err.message)
+    // if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
+    // if (this.firebaseService.userData) this.user = this.firebaseService.userData;
   }
 
 }
