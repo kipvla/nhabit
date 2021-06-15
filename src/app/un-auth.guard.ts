1491dc5e8a6dd2cc62bdfe67842566db6dd1672d
Kip Riecken <kip.riecken@gmail.com>
2021-06-15 18:54:21 -0400

    prevent un-auth guard from redirecting when unnecessary

--- src/app/un-auth.guard.ts
+++ src/app/un-auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRoute, CanActivate, Router } from '@angular/router';
 import { FirebaseService } from './services/firebase.service';
 
 @Injectable({
@@ -8,15 +8,28 @@ import { FirebaseService } from './services/firebase.service';
 export class UnAuthGuard implements CanActivate {
   constructor(
     private firebaseService: FirebaseService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
     ) { }
   canActivate(): boolean {
-    if (!this.firebaseService.isLoggedIn) {
-      return true;
-    } else {
-      this.router.navigate(['/home'])
-      return false;
-    }
+    if (!this.firebaseService.firebaseAuth.authState.subscribe(res => {
+      if (res && res.uid) {
+            console.log('logged in un-auth')
+            return true;
+          } else {
+            console.log('not logged in un-auth')
+            return false;
+          }
+    })) {
+      console.log(this.router.url)
+      this.router.navigate(['/home']);
+      return false
+    };
+    console.log(this.router.url)
+    return true;
+    // console.log(isLoggedIn)
+    // if (!isLoggedIn) return true;
+    // return false;
   }
   
 }
