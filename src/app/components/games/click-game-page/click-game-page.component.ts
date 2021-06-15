edaac458fcd3ac357fb90ccfcdf4b8c2103de97d
Kip Riecken <kip.riecken@gmail.com>
2021-06-15 11:39:33 -0400

    add setTimeout to buttons in click-game-page, add inputs from click-game component

--- src/app/components/games/click-game-page/click-game-page.component.ts
+++ src/app/components/games/click-game-page/click-game-page.component.ts
@@ -7,10 +7,24 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ClickGamePageComponent implements OnInit {
 
-  @Input() id!:string; 
+  @Input() text!:string;
+  @Input() delay!:string;
+  @Input() id!:string;
+  @Input() device!:string;
+  @Input() last!:string;
+
+  time = Number(this.delay);
+
+  disabled = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    const delay = Number(this.delay);
+    setTimeout(() => {
+      console.log('delayClick called')
+      this.disabled = false;
+    }, delay)
   }
 
 }
