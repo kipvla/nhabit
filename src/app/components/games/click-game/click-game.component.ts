ea7915cc0648b5099ad2060aeb6b1a1a1694e577
Kip Riecken <kip.riecken@gmail.com>
2021-06-15 11:35:07 -0400

    clean up sequential page logic, move to reusable animation

--- src/app/components/games/click-game/click-game.component.ts
+++ src/app/components/games/click-game/click-game.component.ts
@@ -1,21 +1,16 @@
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, transition, style, animate, useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-// import {slider} from '../../../animations/animation';
+import {slider, fadeIn, fadeOut} from '../../../animations/animation';
 
 @Component({
   selector: 'app-click-game',
   templateUrl: './click-game.component.html',
   styleUrls: ['./click-game.component.scss'],
   animations: [
-    trigger('carouselAnimation', [
-      transition('void => *', [
-        style({ opacity: 0 }),
-        animate('300ms', style({ opacity: 1 }))
-      ]),
-      transition('* => void', [
-        animate('300ms', style({ opacity: 0 }))
-      ])
+    trigger("carouselAnimation", [
+      transition("void => *", [useAnimation(fadeIn, {params: {time: '800ms'}})]),
+      transition("* => void", [useAnimation(fadeOut, {params: {time: '400ms'}})]),
     ])
   ]
 })
@@ -26,33 +21,22 @@ export class ClickGameComponent implements OnInit {
   pageId = 0;
 
   public slides = [
-    { src: 0 },
-    { src: 1 },
-    { src: 2 },
-    { src: 3 },
-    { src: 4 }
+    { no: 0, text: `For the next minute, let\'s explore interacting with your ${ this.device } in a new way`, delay: 3000 },
+    { no: 1, text: 'We will click the above button just a bit more mindfully', delay: 700 },
+    { no: 2, text: 'In fact, we will be a bit cheeky and not let you click until I say so 😏', delay: 1000 },
+    { no: 3, text: 'This is important, as we spend most of our time on the computer mindlessly clicking', delay: 500 },
+    { no: 4, text: '', delay: 500 },
+    { no: 5, text: '', delay: 500 },
+    { no: 6, text: 'Thanks for playing', delay: 500 },
   ];
 
-  constructor(private activatedRoute: ActivatedRoute,
-    private router: Router) { 
-    this.pageId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
-    // activatedRoute.params.subscribe(val => {
-    //   this.router.navigate([`/game/click/${this.pageId}`]);
-    // })
-  }
+  constructor(){}
 
   ngOnInit(): void {
   }
 
-  // nextPage(): void {
-  //   this.pageId++;
-  //   console.log(this.pageId);
-  //   this.router.navigate([`/game/click/${this.pageId}`]);
-  // }
-
   onNextClick() {
-    const next = this.pageId + 1;
-    this.pageId = next;
+    this.pageId++;
     console.log("next clicked, new current slide is: ", this.pageId);
   }
 
