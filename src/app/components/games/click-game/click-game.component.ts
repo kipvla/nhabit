3fe8d9c5185a5f6a8d9f0b82adbba81e93abfbe8
Kip Riecken <kip.riecken@gmail.com>
2021-06-15 14:03:51 -0400

    clean up animation import, simplify click-through logic, populate slides and add properties in click game component

--- src/app/components/games/click-game/click-game.component.ts
+++ src/app/components/games/click-game/click-game.component.ts
@@ -1,7 +1,6 @@
-import { trigger, transition, style, animate, useAnimation } from '@angular/animations';
+import { trigger, transition, useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import {slider, fadeIn, fadeOut} from '../../../animations/animation';
+import {fadeIn, fadeOut} from '../../../animations/animation';
 
 @Component({
   selector: 'app-click-game',
@@ -18,16 +17,37 @@ export class ClickGameComponent implements OnInit {
 
   device = window.innerWidth < 768 ? 'phone' : 'computer';
 
-  pageId = 0;
+  slideNo = 0;
 
   public slides = [
-    { no: 0, text: `For the next minute, let\'s explore interacting with your ${ this.device } in a new way`, delay: 3000 },
-    { no: 1, text: 'We will click the above button just a bit more mindfully', delay: 700 },
-    { no: 2, text: 'In fact, we will be a bit cheeky and not let you click until I say so 😏', delay: 1000 },
-    { no: 3, text: 'This is important, as we spend most of our time on the computer mindlessly clicking', delay: 500 },
-    { no: 4, text: '', delay: 500 },
-    { no: 5, text: '', delay: 500 },
-    { no: 6, text: 'Thanks for playing', delay: 500 },
+    { text: `For the next minute, let\'s explore interacting with your ${ this.device } in a new way`, delay: 3000, hidingBtn: null },
+    { text: 'We will practice clicking the above button just a bit more mindfully', delay: 700, hidingBtn: null },
+    { text: '(Practice click)', delay: 700, hidingBtn: null },
+    { text: '(Practice click #2)', delay: 700, hidingBtn: null },
+    { text: 'Hey, you\'re pretty good at this!', delay: 1000, hidingBtn: null, btnText: 'Of course I am 😒' },
+    { text: 'But I did say mindfully, not mindlessly, no? 😏', delay: 6000, hidingBtn: true },
+    { text: 'What was it like when the button wasn\'t there?', delay: 1000, hidingBtn: null },
+    { text: 'If you\'re anything like me, you\'ve likely already done plenty of mindless clicking today', delay: 500, hidingBtn: null },
+    { text: 'Websites are rarely as rude as I was just now...', delay: 500, hidingBtn: null },
+    { text: '...hiding the button they want you to click', delay: 500, hidingBtn: null },
+    { text: 'But that does\'nt mean we have to be automatic clicking robots', delay: 500, hidingBtn: null },
+    { text: 'So what if for just a short time, we see if we can\'t do it with a little more care', delay: 500, hidingBtn: null },
+    { text: 'Let\'s try it', delay: 500, hidingBtn: null },
+    { text: 'First, where are you looking?', delay: 5000, hidingBtn: null },
+    { text: 'Are you aware of the size of the room or space you\'re in?', delay: 5000, hidingBtn: null },
+    { text: 'Can you soften your eyes before you click again?', delay: 5000, hidingBtn: null },
+    { text: 'Can you feel the size of the room before you click again?', delay: 5000, hidingBtn: null },
+    { text: 'The space around you...', delay: 5000, hidingBtn: null },
+    { text: '...above you...', delay: 5000, hidingBtn: null },
+    { text: '...under you', delay: 5000, hidingBtn: null },
+    { text: 'Are you leaning into the screen?', delay: 2000, hidingBtn: null },
+    { text: 'If you are, is it necessary?', delay: 5000, hidingBtn: null },
+    { text: 'Now, simply imagine yourself clicking. What do you notice?', delay: 5000, hidingBtn: true },
+    { text: 'Again, can you soften your eyes? Feel the space around you?', delay: 5000, hidingBtn: true },
+    { text: 'There is no \'right\' or \'wrong\' way to click', delay: 5000, hidingBtn: true },
+    { text: 'But I do think we can always find better ways of doing so', delay: 5000, hidingBtn: true },
+    { text: 'That\'s all. Thanks for playing, come back soon!', delay: 500, hidingBtn: null },
+    { text: 'That\'s all. Thanks for playing, come back soon!', delay: 500, hidingBtn: null },
   ];
 
   constructor(){}
@@ -36,8 +56,7 @@ export class ClickGameComponent implements OnInit {
   }
 
   onNextClick() {
-    this.pageId++;
-    console.log("next clicked, new current slide is: ", this.pageId);
+    this.slideNo++;
   }
 
 }
